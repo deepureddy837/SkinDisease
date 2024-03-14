66b0a774bfcd5d1cf16a64857ba7637dc95b73b0
deepureddy837 <deepureddy837@gmail.com>
2024-03-14 22:00:13 +0530

    commit

--- app.py
+++ app.py
@@ -1,32 +1,26 @@
-from flask import Flask, render_template, request, flash, jsonify,get_flashed_messages,Response,session,redirect,url_for
-import cv2, os
+from flask import Flask, render_template, request, flash, jsonify, get_flashed_messages, Response, session, redirect, url_for
+import cv2
+import os
 from ultralytics import YOLO
 from flask import abort
 import pandas as pd
 import mysql.connector
 from flask_session import Session
-from key import secret_key,salt,salt2
+from key import secret_key, salt, salt2
 from itsdangerous import URLSafeTimedSerializer
 from stoken import token
 from cmail import sendmail
 import mysql.connector.pooling
+import re  # Import regular expression module for pattern matching
 
 app = Flask(__name__)
 app.secret_key = b'filesystem'
-app.config['SESSION_TYPE']='filesystem'
+app.config['SESSION_TYPE'] = 'filesystem'
 
-#db=os.environ['RDS_DB_NAME']
-#user=os.environ['RDS_USERNAME']
-#password=os.environ['RDS_PASSWORD']
-#host=os.environ['RDS_HOSTNAME']
-#port=os.environ['RDS_PORT']
-
-#conn=mysql.connector.pooling.MySQLConnectionPool(host=host,user=user,password=password,db=db,port=port,pool_name='DED',pool_size=3,pool_reset_session=True)
-
-conn=mysql.connector.pooling.MySQLConnectionPool(host='localhost',user='root',password="Deepureddy@837",db='skin',pool_name='DED',pool_size=3, pool_reset_session=True)
+conn = mysql.connector.pooling.MySQLConnectionPool(host='localhost', user='root', password="Deepureddy@837", db='skin', pool_name='DED', pool_size=3, pool_reset_session=True)
 
 try:
-    mydb=conn.get_connection()
+    mydb = conn.get_connection()
     cursor = mydb.cursor(buffered=True)
     cursor.execute('CREATE TABLE IF NOT EXISTS users (uid INT PRIMARY KEY auto_increment, username VARCHAR(50), password VARCHAR(20), email VARCHAR(60))')
 
@@ -35,6 +29,7 @@ except Exception as e:
 finally:
     if mydb.is_connected():
         mydb.close()
+
 # Load the pre-trained YOLO model
 model = YOLO('best.pt')
 
@@ -42,25 +37,25 @@ model = YOLO('best.pt')
 with open("coco.txt", "r") as my_file:
     class_list = my_file.read().split("\n")
 
-@app.route('/login',methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if session.get('username'):
         return redirect(url_for('index'))
-    if request.method=='POST':
+    if request.method == 'POST':
         print(request.form)
-        name=request.form['name']
-        password=request.form['password']
+        name = request.form['name']
+        password = request.form['password']
         try:
-            mydb=conn.get_connection()
-            cursor=mydb.cursor(buffered=True)
+            mydb = conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
         except Exception as e:
             print(e)
         else:
-            cursor.execute('SELECT count(*) from users where username=%s and password=%s',[name,password])
-            count=cursor.fetchone()[0]
+            cursor.execute('SELECT count(*) from users where username=%s and password=%s', [name, password])
+            count = cursor.fetchone()[0]
             cursor.close()
-            if count==1:
-                session['username']=name
+            if count == 1:
+                session['username'] = name
                 return redirect(url_for('index'))
             else:
                 flash('Invalid username or password')
@@ -70,7 +65,6 @@ def login():
                 mydb.close()
     return render_template('login.html')
 
-import re  # Import regular expression module for pattern matching
 
 @app.route('/registration', methods=['GET', 'POST'])
 def registration():
@@ -108,7 +102,7 @@ def registration():
             elif count_email == 1:
                 flash('Email already in use')
                 return render_template('registration.html')
-            
+
             data = {'username': username, 'password': password, 'email': email}
             subject = 'Email Confirmation'
             body = f"Thanks for signing up\n\nFollow this link for further steps: {url_for('confirm', token=token(data, salt), _external=True)}"
@@ -124,27 +118,28 @@ def registration():
 @app.route('/confirm/<token>')
 def confirm(token):
     try:
-        serializer=URLSafeTimedSerializer(secret_key)
-        data=serializer.loads(token,salt=salt,max_age=180)
+        serializer = URLSafeTimedSerializer(secret_key)
+        data = serializer.loads(token, salt=salt, max_age=180)
     except Exception as e:
-        #print(e)
+        # print(e)
         return 'Link Expired register again'
     else:
         try:
-            mydb=conn.get_connection()
-            cursor=mydb.cursor(buffered=True)
+            mydb = conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
         except Exception as e:
             print(e)
         else:
-            username=data['username']
-            cursor.execute('select count(*) from users where username=%s',[username])
-            count=cursor.fetchone()[0]
-            if count==1:
+            username = data['username']
+            cursor.execute('select count(*) from users where username=%s', [username])
+            count = cursor.fetchone()[0]
+            if count == 1:
                 cursor.close()
-                flash('You are already registerterd!')
+                flash('You are already registered!')
                 return redirect(url_for('login'))
             else:
-                cursor.execute('insert into users(username,password,email) values(%s,%s,%s)',(data['username'], data['password'], data['email']))
+                cursor.execute('insert into users(username,password,email) values(%s,%s,%s)',
+                               (data['username'], data['password'], data['email']))
                 mydb.commit()
                 cursor.close()
                 flash('Details registered! You can Login now')
@@ -154,28 +149,28 @@ def confirm(token):
                 mydb.close()
 
 
-@app.route('/forget',methods=['GET','POST'])
+@app.route('/forget', methods=['GET', 'POST'])
 def forgot():
-    if request.method=='POST':
-        email=request.form['email']
+    if request.method == 'POST':
+        email = request.form['email']
         try:
-            mydb=conn.get_connection()
-            cursor=mydb.cursor(buffered=True)
+            mydb = conn.get_connection()
+            cursor = mydb.cursor(buffered=True)
         except Exception as e:
             print(e)
         else:
-            cursor.execute('select count(*) from users where email=%s',[email])
-            count=cursor.fetchone()[0]
+            cursor.execute('select count(*) from users where email=%s', [email])
+            count = cursor.fetchone()[0]
             cursor.close()
-            if count==1:
-                cursor=mydb.cursor(buffered=True)
-                cursor.execute('SELECT email from users where email=%s',[email])
-                status=cursor.fetchone()[0]
+            if count == 1:
+                cursor = mydb.cursor(buffered=True)
+                cursor.execute('SELECT email from users where email=%s', [email])
+                status = cursor.fetchone()[0]
                 cursor.close()
-                subject='Forget Password'
-                confirm_link=url_for('reset',token=token(email,salt=salt2),_external=True)
-                body=f"Use this link to reset your password-\n\n{confirm_link}"
-                sendmail(to=email,body=body,subject=subject)
+                subject = 'Forget Password'
+                confirm_link = url_for('reset', token=token(email, salt=salt2), _external=True)
+                body = f"Use this link to reset your password-\n\n{confirm_link}"
+                sendmail(to=email, body=body, subject=subject)
                 flash('Reset link sent check your email')
                 return redirect(url_for('login'))
             else:
@@ -187,25 +182,31 @@ def forgot():
     return render_template('forgot.html')
 
 
-@app.route('/reset/<token>',methods=['GET','POST'])
+@app.route('/reset/<token>', methods=['GET', 'POST'])
 def reset(token):
     try:
-        serializer=URLSafeTimedSerializer(secret_key)
-        email=serializer.loads(token,salt=salt2,max_age=180)
+        serializer = URLSafeTimedSerializer(secret_key)
+        email = serializer.loads(token, salt=salt2, max_age=180)
     except:
-        abort(404,'Link Expired')
+        abort(404, 'Link Expired')
     else:
-        if request.method=='POST':
-            newpassword=request.form['npassword']
-            confirmpassword=request.form['cpassword']
-            if newpassword==confirmpassword:
+        if request.method == 'POST':
+            newpassword = request.form['npassword']
+            confirmpassword = request.form['cpassword']
+            if newpassword == confirmpassword:
+                # Check password constraints
+                password_pattern = r'^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+])[a-zA-Z0-9!@#$%^&*()_+]{8,}$'
+                if not re.match(password_pattern, newpassword):
+                    flash('Password must be 8 characters long and contain at least one uppercase letter, one number, and one special character.')
+                    return render_template('newpassword.html')
+
                 try:
-                    mydb=conn.get_connection()
-                    cursor=mydb.cursor(buffered=True)
+                    mydb = conn.get_connection()
+                    cursor = mydb.cursor(buffered=True)
                 except Exception as e:
                     print(e)
                 else:
-                    cursor.execute('update users set password=%s where email=%s',[newpassword,email])
+                    cursor.execute('update users set password=%s where email=%s', [newpassword, email])
                     mydb.commit()
                     flash('Reset Successful')
                     return redirect(url_for('login'))
@@ -215,8 +216,9 @@ def reset(token):
             else:
                 flash('Passwords mismatched')
                 return render_template('newpassword.html')
-        #flash.clear()
+        # flash.clear()
         return render_template('newpassword.html')
+
 
 @app.route('/logout')
 def logout():
@@ -246,6 +248,7 @@ def index():
         return render_template('index.html')
     else:
         return redirect(url_for('login'))
+
 
 @app.route('/detect_disease/<path:file_path>/<filename>', methods=['GET', 'POST'])
 def detect_disease(file_path, filename):
@@ -278,13 +281,14 @@ def detect_disease(file_path, filename):
         output_path = f'static/detections/{filename}'
         os.makedirs(os.path.dirname(output_path), exist_ok=True)
         cv2.imwrite(output_path, frame)
-        #flash('Disease Detected Successfully.', 'success')
+        # flash('Disease Detected Successfully.', 'success')
         # Get flashed messages
         flash_messages = get_flashed_messages(with_categories=True)
         flash_messages = [{'category': msg[0], 'message': msg[1]} for msg in flash_messages]
         return jsonify({'output_path': output_path, 'diseases': list(diseases), 'filename': filename, 'flash_messages': flash_messages})
     else:
         return redirect(url_for('login'))
+
 
 @app.route('/get_medicines/<diseases>/<filename>', methods=['GET', 'POST'])
 def get_medicines(diseases, filename):
@@ -342,7 +346,6 @@ def video_feed():
         return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
     else:
         return redirect(url_for('login'))
-
 
 
 if __name__ == '__main__':
